710efb13842186dc4069f314f6ed02b275c91053
Yashita Kharbanda <yashitakharbanda2505@gmail.com>
2026-06-15 14:58:01 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -19,8 +19,9 @@ connection
     app.use(
       cors({
         origin:
-          "*",
-          credentials: true,
+          process.env.CLIENT_URL ||
+          "http://localhost:5173",
+        credentials: true,
       })
     );
     app.use(express.json());
